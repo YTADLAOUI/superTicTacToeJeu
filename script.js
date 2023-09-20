7595ca0a88d71fbdfac936ac832e6af91ba3fc05
youssef tadlaoui <usftadlaoui@gmail.com>
2023-09-20 16:19:48 +0100

    circle&cross

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 const containerTic= document.querySelector("#gameTic");
 const infoTic=document.querySelector("#infoTic");
 const borderses= Array(20).fill(null).map(()=>Array(20).fill(null));
+let marke="circle"
 const container=()=>{
+let count=0;
   borderses.forEach((borders,index)=>{
-        // const firstSquare=document.createElement("div");
-        // firstSquare.classList.add("square");
-        // containerTic.append(firstSquare);
         borders.forEach((border,index)=>{
           const cellElement=document.createElement("div");
           cellElement.classList.add("square");
+          cellElement.id=count;
           containerTic.append(cellElement);
+          count++
+          cellElement.addEventListener("click",addMark);
+          
         })
+      })
 
-  })
 }
 container();
+
+function addMark(e){
+const displayMark=document.createElement("div");
+displayMark.classList.add(marke);
+e.target.append(displayMark);
+marke = marke==="circle"? "cross": "circle";
+  infoTic.textContent=`maintenant est ${marke}`;
+}
