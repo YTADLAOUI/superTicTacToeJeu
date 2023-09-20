babf30e104a30582737ab7aa25495e7b76ae90bc
youssef tadlaoui <usftadlaoui@gmail.com>
2023-09-20 11:57:16 +0100

    creeSquare

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const containerTic= document.querySelector("#gameTic");
 const infoTic=document.querySelector("#infoTic");
-const borders= Array(20).fill(null).map(()=>Array(20).fill(null));
+const borderses= Array(20).fill(null).map(()=>Array(20).fill(null));
 const container=()=>{
-  borders.forEach((border,index)=>{
-   const cellElement=document.createElement("div")
-    cellElement.classList.add("square");
-    containerTic.append(cellElement);
+  borderses.forEach((borders,index)=>{
+        // const firstSquare=document.createElement("div");
+        // firstSquare.classList.add("square");
+        // containerTic.append(firstSquare);
+        borders.forEach((border,index)=>{
+          const cellElement=document.createElement("div");
+          cellElement.classList.add("square");
+          containerTic.append(cellElement);
+        })
 
   })
 }
