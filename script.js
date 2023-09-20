df2297973ac664a2d8bad5d79e3ede9f0fd67cf4
youssef tadlaoui <usftadlaoui@gmail.com>
2023-09-20 20:02:00 +0100

    stock dans array &&sans repitition de click dans la meme cell

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
-const containerTic= document.querySelector("#gameTic");
-const infoTic=document.querySelector("#infoTic");
-const borderses= Array(20).fill(null).map(()=>Array(20).fill(null));
-let marke="circle"
-const container=()=>{
-let count=0;
-  borderses.forEach((borders,index)=>{
-        borders.forEach((border,index)=>{
-          const cellElement=document.createElement("div");
-          cellElement.classList.add("square");
-          cellElement.id=count;
-          containerTic.append(cellElement);
-          count++
-          cellElement.addEventListener("click",addMark);
-          
-        })
-      })
+const containerTic = document.querySelector("#gameTic");
+const infoTic = document.querySelector("#infoTic");
+const borderses = Array(20).fill(null).map(() => Array(20).fill(null));
+let marke = "circle";
+const container = () => {
+    let count = 1;
+    borderses.forEach((borders, x) => {
+        borders.forEach((border, y) => {
+            const cellElement = document.createElement("div");
+            cellElement.classList.add("square");
+            cellElement.id = count;
+            containerTic.appendChild(cellElement);
+            count++;
 
-}
+            cellElement.addEventListener("click", function (e) {
+                if (!borderses[x][y] && !e.target.querySelector(".circle") && !e.target.querySelector(".cross")) {
+                    const displayMark = document.createElement("div");
+                    displayMark.classList.add(marke);
+                    e.target.appendChild(displayMark);
+                    borderses[x][y] = marke;
+                        marke = marke === "circle" ? "cross" : "circle";
+                        infoTic.textContent = `C'est maintenant au tour de ${marke}`;
+                    
+                }
+            });
+        });
+    });
+};
+
 container();
-
-function addMark(e){
-const displayMark=document.createElement("div");
-displayMark.classList.add(marke);
-e.target.append(displayMark);
-marke = marke==="circle"? "cross": "circle";
-  infoTic.textContent=`maintenant est ${marke}`;
-}
