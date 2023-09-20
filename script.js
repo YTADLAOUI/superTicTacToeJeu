94f4d444c28caedd5263b8b144d50fd6bdfe3ac7
youssef tadlaoui <usftadlaoui@gmail.com>
2023-09-20 20:48:52 +0100

    checkenDinner

--- script.js
+++ script.js
@@ -2,6 +2,24 @@ const containerTic = document.querySelector("#gameTic");
 const infoTic = document.querySelector("#infoTic");
 const borderses = Array(20).fill(null).map(() => Array(20).fill(null));
 let marke = "circle";
+let gameOver = false;
+
+function chickenDinner(marke){
+  console.log(marke)
+//  horizantal
+for(let i=0; i<20; i++){
+  let count=0
+  for(let j=0;j<20;j++){
+    if(borderses[i][j]===marke){
+      count++
+      if(count===5)return true;
+    }else{
+      count=0;
+    }
+  }
+}
+
+}
 const container = () => {
     let count = 1;
     borderses.forEach((borders, x) => {
@@ -13,14 +31,19 @@ const container = () => {
             count++;
 
             cellElement.addEventListener("click", function (e) {
-                if (!borderses[x][y] && !e.target.querySelector(".circle") && !e.target.querySelector(".cross")) {
+                if (!gameOver && !borderses[x][y] && !e.target.querySelector(".circle") && !e.target.querySelector(".cross")) {
                     const displayMark = document.createElement("div");
                     displayMark.classList.add(marke);
                     e.target.appendChild(displayMark);
                     borderses[x][y] = marke;
-                        marke = marke === "circle" ? "cross" : "circle";
-                        infoTic.textContent = `C'est maintenant au tour de ${marke}`;
-                    
+                    console.log(borderses)
+                    if(chickenDinner(marke)){
+                      infoTic.textContent = `Le joueur ${marke} a gagné !`;
+                      gameOver=true;
+                    }else{ 
+                      marke = marke === "circle" ? "cross" : "circle";
+                    infoTic.textContent = `C'est maintenant au tour de ${marke}`;
+                  } 
                 }
             });
         });
