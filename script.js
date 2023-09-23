218dd2a25acfd7981a3c14d7e6ac8b9bc6f3e45d
Youssef tdl <usftadlaoui@gmail.com>
2023-09-23 13:45:09 +0100

    fix errur in diagonal

--- script.js
+++ script.js
@@ -1,71 +1,71 @@
 const containerTic = document.querySelector("#gameTic");
 const infoTic = document.querySelector("#infoTic");
-const rest= document.querySelector("#rest");
+const rest = document.querySelector("#rest");
 const borderses = Array(20).fill(null).map(() => Array(20).fill(null));
 console.log(borderses)
 let marke = "circle";
 let gameOver = false;
 // arr=[];
-function chickenDinner(marke){
-//  horizantal
-      for(let i=0; i<20; i++){
-        let count=0;
-        for(let j=0;j<20;j++){
-          if(borderses[i][j]===marke){
-            // arr.push([i,j]);
-            count++
-            // console.log(arr)
-            if(count===5)return true;
-          }else{
-            count=0;
-          }
-        }
-        
-      }
-     
-//virticale
-      for(let i=0; i<20; i++){
-        let count=0;
-        for(let j=0;j<20;j++){
-          if(borderses[j][i]===marke){
-
-            count++
-            if(count===5)return true;
-          }else{
-            count=0
-          }
-        }
-      }
-// diagonal-gouchDroit
-      for(let i =0; i<20; i++){
-        let count=1;
-        for(let j=0; j<20 ;j++){
-          for(let k=0; k<5; k++){
-            if(i+k<20 && j+k<20 && borderses[i+k][j+k]===marke){
-                  count++;
-                  if(count==5)return true
-            }else{
-            count=0
+function chickenDinner(marke) {
+    //  horizantal
+    for (let i = 0; i < 20; i++) {
+        let count = 0;
+        for (let j = 0; j < 20; j++) {
+            if (borderses[i][j] === marke) {
+                // arr.push([i,j]);
+                count++
+                // console.log(arr)
+                if (count === 5) return true;
+            } else {
+                count = 0;
             }
-          }
         }
-      }
-// diagonal-droitGouche
-      for(let i=0; i<20 ;i++){
-      count=1;
-      for(let j=0; j<20; j++){
-          for(let k=0;k<20;k++){
-            if( i+k<20 && j-k>=0 && borderses[i+k][j-k]===marke){
-              count++
+    }
 
-              if(count===5)return true;
-            }else{
-              count=0;
+    //virticale
+    for (let i = 0; i < 20; i++) {
+        let count = 0;
+        for (let j = 0; j < 20; j++) {
+            if (borderses[j][i] === marke) {
+
+                count++
+                if (count === 5) return true;
+            } else {
+                count = 0
             }
-          }
         }
-      }
-return false
+    }
+    // diagonal-gouchDroit
+    for (let i = 0; i < 20; i++) {
+        let count = 0;
+        for (let j = 0; j < 20; j++) {
+            for (let k = 0; k < 5; k++) {
+                if (i + k < 20 && j + k < 20 && borderses[i + k][j + k] === marke) {
+                    count++;
+                    if (count === 5) return true;
+                } else {
+                    count = 0;
+                    break
+                }
+            }
+        }
+    }
+    // diagonal-droitGouche
+    for (let i = 0; i < 20; i++) {
+        let count = 0;
+        for (let j = 0; j < 20; j++) {
+            for (let k = 0; k < 20; k++) {
+                if (i + k < 20 && j - k > 0 && borderses[i + k][j - k] === marke) {
+                    count++
+                    if (count === 5) return true;
+                } else {
+                    count = 0;
+                    break
+                }
+            }
+        }
+    }
+    return false
 }
 const container = () => {
     let count = 1;
@@ -75,38 +75,38 @@ const container = () => {
             cellElement.classList.add("square");
             cellElement.id = count;
             containerTic.appendChild(cellElement);
-             count++;
-            cellElement.addEventListener("click", function (e) {
+            count++;
+            cellElement.addEventListener("click", function(e) {
                 if (!gameOver && !borderses[x][y] && !e.target.querySelector(".circle") && !e.target.querySelector(".cross")) {
                     const displayMark = document.createElement("div");
                     displayMark.classList.add(marke);
                     e.target.appendChild(displayMark);
                     borderses[x][y] = marke
 
-                    if(chickenDinner(marke)){
-                      infoTic.textContent = `Le joueur ${marke} a gagné !`;
+                    if (chickenDinner(marke)) {
+                        infoTic.textContent = `Le joueur ${marke} a gagné !`;
 
-                      gameOver=true;
-                    }else{ 
-                      marke = marke === "circle" ? "cross" : "circle";
-                    infoTic.textContent = `C'est maintenant au tour de ${marke}`;
-                  } 
+                        gameOver = true;
+                    } else {
+                        marke = marke === "circle" ? "cross" : "circle";
+                        infoTic.textContent = `C'est maintenant au tour de ${marke}`;
+                    }
                 }
             });
         });
     });
 };
-rest.addEventListener("click",()=>{
-  let cellCircle =document.querySelectorAll(".circle");
-  let cellCroiss=document.querySelectorAll(".cross");
-  cellCircle.forEach(el=>el.classList.remove('circle'));
-  cellCroiss.forEach(el=>el.classList.remove('cross'));
-  borderses.forEach((borders,x)=>{
-    borders.forEach(
-      (border,y)=>{
-        borderses[x][y]=null;
-      }
-      )
+rest.addEventListener("click", () => {
+    let cellCircle = document.querySelectorAll(".circle");
+    let cellCroiss = document.querySelectorAll(".cross");
+    cellCircle.forEach(el => el.classList.remove('circle'));
+    cellCroiss.forEach(el => el.classList.remove('cross'));
+    borderses.forEach((borders, x) => {
+        borders.forEach(
+            (border, y) => {
+                borderses[x][y] = null;
+            }
+        )
     })
     gameOver = false;
     marke = "circle";
